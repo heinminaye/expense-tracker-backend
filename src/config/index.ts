d2ab898d3fd98d578cc05a9f242e5995ebb83762
heinminaye <heinminaye8000@gmail.com>
2023-09-01 22:19:59 +0630

    changenumberrate

--- src/config/index.ts
+++ src/config/index.ts
@@ -24,7 +24,7 @@ export default {
   //Postgrpsql Local DB config
   HOST: "localhost",
   USER: "postgres",
-  PASSWORD: "Zawnay2001",
+  PASSWORD: "santoe",
   DB: "sales",
   dialect: "postgres",
   pool: {
